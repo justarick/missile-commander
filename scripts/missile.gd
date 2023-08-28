cec84c3813c4547e76e1115a45199a1f56079347
justarick <90181703+justarick@users.noreply.github.com>
2023-08-28 15:49:44 +0200

    Refine trail-effect

--- scripts/missile.gd
+++ scripts/missile.gd
@@ -1,37 +1,39 @@
 extends Area2D
 
 
+@export var is_friendly : bool
 @export var speed : float = 50.0
 @export var direction : Vector2
-@export var is_friendly : bool
 
 
 @onready var main_scene = get_tree().current_scene
-@onready var trail_effect_scene : PackedScene = preload("res://effects/trail_effect.tscn")
+
+@onready var trail = $TrailEffect
 
 
 func _ready():
-	spawn_trail_effect()
+	configure_trail()
 
 
 func _physics_process(delta):
-	position += direction * speed * delta
+	move(delta)
+	update_trail()
 
 
 func _on_screen_exited():
 	queue_free()
 
 
-func spawn_trail_effect():
-	var trail_effect = trail_effect_scene.instantiate()
-	
-	trail_effect.direction = direction
-	trail_effect.speed = speed
-	trail_effect.global_position = global_position
-	
+func move(delta):
+	position += direction * speed * delta
+
+
+func update_trail():
+	trail.add_point(position)
+
+
+func configure_trail():
 	if is_friendly:
-		trail_effect.default_color = Color(1, 1, 1, 1)
+		trail.default_color = Color.WHITE
 	else:
-		trail_effect.default_color = Color(1, 0, 0, 1)
-	
-	main_scene.add_child(trail_effect)
+		trail.default_color = Color.RED
