2db3e98a8bc707b049add88af959f3251a2725b2
justarick <90181703+justarick@users.noreply.github.com>
2023-08-28 09:34:00 +0200

    Refactor missile_handler.gd

--- scripts/missile_handler.gd
+++ scripts/missile_handler.gd
@@ -2,52 +2,57 @@ extends Node2D
 
 
 @onready var main_scene : Node2D = get_tree().current_scene
-@onready var missile_scene : PackedScene = preload("res://prefabs/missile.tscn")
-@onready var friendly_missile_img : CompressedTexture2D = preload("res://assets/images/missile.png")
-@onready var hostile_missile_img : CompressedTexture2D = preload("res://assets/images/enemy_missile.png")
-@onready var marker_position : Vector2 = $Marker.global_position
 @onready var screen_size : Vector2 = get_viewport_rect().size
+
+@onready var marker_position : Vector2 = $Marker.global_position
+
+@onready var missile_scene : PackedScene = preload("res://prefabs/missile.tscn")
 
 
 func _ready(): pass
 
 
-func _process(delta):
+func _process(_delta):
 	if Input.is_action_just_pressed("shoot"):
-		spawn_friendly_missile()
+		spawn_missile(true)
 
 
 func _on_hostile_missile_countdown_timeout():
-	spawn_enemy_missile()
+	spawn_missile(false)
 
 
-func spawn_friendly_missile():
+func spawn_missile(is_friendly : bool) -> void:
 	var missile : Area2D = missile_scene.instantiate()
-	var spawn_point : Vector2 = marker_position
-	var direction : Vector2 = get_global_mouse_position() - marker_position
 	
-	direction = direction.normalized()
-	
-	missile.global_position = spawn_point
-	missile.direction = direction
-	missile.is_friendly = true
+	missile.is_friendly = is_friendly
+	missile.position = configure_missile_position(missile.is_friendly)
+	missile.direction = configure_missile_direction(missile.is_friendly, missile.position)
 	
 	main_scene.add_child(missile)
 
 
-func spawn_enemy_missile():
-	var random : RandomNumberGenerator = RandomNumberGenerator.new()
+func configure_missile_position(is_friendly : bool) -> Vector2:
+	var missile_position : Vector2
+	
+	if is_friendly:
+		missile_position = marker_position
+	else:
+		var random : RandomNumberGenerator = RandomNumberGenerator.new()
+		missile_position = Vector2(random.randf() * screen_size.x, 0.0)
+	
+	return missile_position
 
-	var missile : Area2D = missile_scene.instantiate()
-	var spawn_point : Vector2 = Vector2(random.randf() * get_viewport_rect().size.x, 0.0)
-	var direction : Vector2
+
+func configure_missile_direction(is_friendly : bool, missile_position) -> Vector2:
+	var missile_direction : Vector2
 	
-	direction.x = random.randf_range(-spawn_point.x, screen_size.x - spawn_point.x)
-	direction.y = screen_size.y
-	direction = direction.normalized()
+	if is_friendly:
+		missile_direction = get_global_mouse_position() - marker_position
+	else:
+		var random : RandomNumberGenerator = RandomNumberGenerator.new()
+		missile_direction.x = random.randf_range(-missile_position.x, screen_size.x - missile_position.x)
+		missile_direction.y = screen_size.y
 	
-	missile.global_position = spawn_point
-	missile.direction = direction
-	missile.is_friendly = false
+	missile_direction = missile_direction.normalized()
 	
-	main_scene.add_child(missile)
+	return missile_direction
